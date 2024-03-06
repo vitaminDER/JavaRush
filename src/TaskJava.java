45972d7b43522dd4b760a63d5bea1a38a7b246f1
Efim Goncharov <efim.goncharov@bk.ru>
2024-03-06 23:35:59 +0500

    Яблоки бывают разные - 12 lvl

---
+++ src/TaskJava.java
@@ -0,0 +1,40 @@
+public class TaskJava {
+    public static void main(String[] args) {
+        CellPhone cellPhone1 = new CellPhone("Pineapple", "piPhone Z");
+        CellPhone cellPhone2 = new CellPhone("Pineapple", "piPhone Z");
+        if (cellPhone1.equals(cellPhone2)) {
+            System.out.println("телефоны одинаковые");
+        }
+    }
+}
+
+class CellPhone {
+    String vendor;
+    String model;
+
+    public CellPhone(String vendor, String model)
+    {
+        this.vendor = vendor;
+        this.model = model;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CellPhone cellPhone = (CellPhone) o;
+
+        return vendor == cellPhone.vendor && model == cellPhone.model;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(vendor, model);
+    }
+}
