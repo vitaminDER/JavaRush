4b636a2b6aa1c5f2d85183eac193014e9ae04096
Efim Goncharov <efim.goncharov@bk.ru>
2024-03-08 02:09:36 +0500

    Составляем список студентов - 12 lvl

---
+++ src/University.java
@@ -0,0 +1,29 @@
+public class University {
+    private final String[] students = new String[30];
+
+    public void addStudent(String student) {
+        int i = 0;
+        for (; i < students.length; i++) {
+            if (students[i] == null) {
+                break;
+            }
+        }
+        students[i] = student;
+    }
+
+    public void printStudents() {
+        for (String student : students) {
+            if (student != null) {
+                System.out.println(student);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        new University().addStudent("Максим Федоренко");
+        new University().addStudent("Олег Кесарчук");
+        new University().printStudents();
+    }
+
+}
