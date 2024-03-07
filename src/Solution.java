bcfab0632d638e5b6335bf149651ef419bc96b5b
Efim Goncharov <efim.goncharov@bk.ru>
2024-03-07 23:40:56 +0500

    Импортный код - 12 lvl

---
+++ src/Solution.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public static void main(String[] args) {
+        java.util.Scanner console = new java.util.Scanner(System.in);
+        if (console.hasNext()) {
+            System.out.println(console.next());
+        }
+    }
+}
