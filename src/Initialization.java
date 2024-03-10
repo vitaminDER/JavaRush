9e2db853118646140ad88fb6db14e15e999c6f63
Efim Goncharov <efim.goncharov@bk.ru>
2024-03-10 22:46:43 +0500

    Забытая инициализация - 12 lvl

---
+++ src/Initialization.java
@@ -0,0 +1,9 @@
+public class Initialization {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = i;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
