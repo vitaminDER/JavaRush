bf35636b264268c05273e71d87a618add63761e1
Efim Goncharov <efim.goncharov@bk.ru>
2024-03-13 23:13:01 +0500

    Ежик и яблоко - 11 lvl

---
+++ src/Hedgehog.java
@@ -0,0 +1,17 @@
+public class Hedgehog {
+    public void eat(Apple apple)
+    {
+        System.out.println("Яблоко было съедено!");
+    }
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Apple apple = new Apple();
+        Hedgehog hedgehog = new Hedgehog();
+        hedgehog.eat(apple);
+    }
+
+    public static class Apple {
+    }
+}
+
