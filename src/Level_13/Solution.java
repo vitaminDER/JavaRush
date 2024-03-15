de837e511b51809ad114b175481857c1e7c394dd
Efim Goncharov <efim.goncharov@bk.ru>
2024-03-15 23:42:13 +0500

    13 lvl

--- src/Level_13/Solution.java
+++ src/Level_13/Solution.java
@@ -1,13 +1,34 @@
 package Level_13;
 
-public class Solution {
-    static Integer first = 1000;
-    static Integer second = 1000;
-    static int third = 1000;
+//public class Solution {
+//    static Integer first = 1000;
+//    static Integer second = 1000;
+//    static int third = 1000;
+//
+//    public static void main(String[] args) {
+//        System.out.println(first.equals(second));
+//        System.out.println(third == second);
+//        System.out.println(third == first);
+//    }
+//}
 
+
+/*Какое число меньше*/
+public class Solution  {
     public static void main(String[] args) {
-        System.out.println(first.equals(second));
-        System.out.println(third == second);
-        System.out.println(third == first);
+        int first = 2;
+        int second = 1;
+
+        Boolean isLess = isLess(first, second);
+
+        if (isLess) {
+            System.out.println("Первое число меньше второго");
+        } else {
+            System.out.println("Первое число больше или равно второму числу");
+        }
+    }
+
+    public static Boolean isLess(int first, int second) {
+        return first < second ? true : false;
     }
 }
