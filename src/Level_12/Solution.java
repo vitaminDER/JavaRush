c4333abbbe72db973d3aa2f99db3172ec714f74e
Efim Goncharov <efim.goncharov@bk.ru>
2024-03-16 23:45:47 +0500

    12 lvl

---
+++ src/Level_12/Solution.java
@@ -0,0 +1,15 @@
+package Level_12;
+
+// ЗАТЕНЕНИЕ ПОЛЯ КЛАССА
+/*public class Solution {
+    public static int salary;
+
+    public static void add(int increase) {
+        //int salary = Solution.salary;
+        salary += increase;
+    }
+    public static void main(String[] args) {
+        add(100500);
+        System.out.println(salary);
+    }
+}*/
