026d459bfedd2234b85db22e02b00200c949e003
Efim Goncharov <efim.goncharov@bk.ru>
2024-03-18 21:02:23 +0500

    13 lvl

--- src/Level_13/Solution.java
+++ src/Level_13/Solution.java
@@ -36,3 +36,12 @@ public class Solution  {
 }
 */
 
+//ПРОЩАЙ, ПАСКАЛЬ
+/*public  class Solution  {
+    public static ArrayList<String> programmingLanguages = new ArrayList<>(Arrays.asList("C", "C++", "Python", "JavaScript", "Ruby", "Java", "Pascal"));
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        programmingLanguages.remove("Pascal");
+    }
+}*/
