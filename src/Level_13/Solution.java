0e69eae023d327a5653c04bca173829533eed556
Efim Goncharov <efim.goncharov@bk.ru>
2024-03-23 21:55:57 +0500

    13 lvl

--- src/Level_13/Solution.java
+++ src/Level_13/Solution.java
@@ -105,4 +105,24 @@ public class Solution {
     }
 }*/
 
+//НАЗАД В ПРОШЛОЕ
+/*
+public class Solution {
+    private int[] intArray = new int[4];
+
+    public int[] getIntArray() {
+        return intArray;
+    }
+
+    public void setIntArray(int[] intArray) {
+        this.intArray = intArray;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String string = new String("Hello");
+        StringBuilder stringBuilder = new StringBuilder();
+    }
+}*/
+
 
