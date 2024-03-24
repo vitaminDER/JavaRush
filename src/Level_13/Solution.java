3e19090be15afd346a566978f16406dc9edd3685
Efim Goncharov <efim.goncharov@bk.ru>
2024-03-24 22:55:24 +0500

    13 lvl

--- src/Level_13/Solution.java
+++ src/Level_13/Solution.java
@@ -141,4 +141,25 @@ public class Solution {
     }
 }*/
 
+//ПЛАТА ПО СЧЕТАМ
+/*
+public class Solution {
+    public static Integer balance = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+        String bill = "1234567890";
+
+        System.out.println("Текущий баланс : " + balance);
+        processPayment(bill);
+        System.out.println("Текущий баланс : " + balance);
+    }
+
+    public static void processPayment(String bill) {
+        //напишите тут ваш код
+        balance = balance - Integer.parseInt(bill);
+
+    }
+}
+*/
+
 
