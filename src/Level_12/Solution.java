673a0a2c8399373dd06ae4f1c3766cd4fe5f2050
Efim Goncharov <efim.goncharov@bk.ru>
2024-03-26 01:29:24 +0500

    12 lvl

--- src/Level_12/Solution.java
+++ src/Level_12/Solution.java
@@ -85,3 +85,37 @@ public class Solution {
         return result;
     }
 }*/
+
+//ПОЧУВСТВУЙ СЕБЯ ДЕКАНОМ
+/*
+import java.util.ArrayList;
+import java.util.List;
+public class UniversityGroup {
+    public List<String> students;
+
+    public UniversityGroup() {
+        students = new ArrayList<>();
+        students.add("Сергей Фрединский");
+        students.add("Виталий Правдивый");
+        students.add("Максим Козыменко");
+        students.add("Наталия Андрющенко");
+        students.add("Ира Величенко");
+        students.add("Николай Соболев");
+        students.add("Снежана Слободенюк");
+    }
+
+    public void exclude(String excludedStudent) {
+        for (int i = 0; i < students.size(); i++) {
+            if (students.get(i).equals(excludedStudent)) {
+                students.remove(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        UniversityGroup universityGroup = new UniversityGroup();
+        universityGroup.exclude("Виталий Правдивый");
+        universityGroup.students.forEach(System.out::println);
+    }
+}
+*/
