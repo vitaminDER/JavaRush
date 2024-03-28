9a68c98c84a05caa56147c1a2f8b7e17b92222f7
Efim Goncharov <efim.goncharov@bk.ru>
2024-03-28 23:29:20 +0500

    13 lvl

--- src/Level_13/Solution.java
+++ src/Level_13/Solution.java
@@ -162,4 +162,27 @@ public class Solution {
 }
 */
 
+//ВЫВОДИМ В КОНСОЛИ РАЗНЫЕ ТИПЫ
+/*
+import java.util.ArrayList;
+public class Solution {
+
+    public static void printAnything(ArrayList arrayList) {
+        for (int i = 0; i < arrayList.size(); i++) {
+            System.out.println(arrayList.get(i));
+        }
+    }
+
+    public static void main(String[] args) {
+        var arrayList = new ArrayList<>();
+        arrayList.add(15);
+        arrayList.add("Hello");
+        arrayList.add(154);
+        arrayList.add("string");
+
+        printAnything(arrayList);
+    }
+}
+*/
+
 
