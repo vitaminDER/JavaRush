d76aa53532b7c5f8cf22e7b865adbaaf256c45e8
Efim Goncharov <efim.goncharov@bk.ru>
2024-03-29 23:06:50 +0500

    13 lvl

--- src/Level_13/Solution.java
+++ src/Level_13/Solution.java
@@ -185,4 +185,37 @@ public class Solution {
 }
 */
 
+//ДЖЕНЕРИКИ И СТУДЕНТЫ
+/*
+import java.util.ArrayList;
+public class UniversityDepartment {
+
+    public static void main(String[] args) {
+        var students = new ArrayList<Student>();
+        students.add(new Student("Акакий"));
+        students.add(new Student("Любослав"));
+
+        printStudentNames(students);
+    }
+
+    public static void printStudentNames(ArrayList<Student> students) {
+        for (int i = 0; i < students.size(); i++) {
+            System.out.println(students.get(i).getName());
+        }
+    }
+
+    public static class Student {
+        private String name;
+
+        public Student(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+}
+*/
+
 
