5777723294a1e1d9b6a44a35a066a88cd69c2bc3
Efim Goncharov <efim.goncharov@bk.ru>
2024-03-30 23:06:39 +0500

    13 lvl

--- src/Level_13/Solution.java
+++ src/Level_13/Solution.java
@@ -218,4 +218,35 @@ public class UniversityDepartment {
 }
 */
 
+//АВТОУПАКОВКА
+/*
+public class Solution {
+    byte byteValue;
+    short shortValue;
+    int intValue;
+    long longValue;
+
+    float floatValue;
+    double doubleValue;
+
+    char charValue;
+    boolean booleanValue;
+
+    Byte byteValueBox = Byte.valueOf(byteValue);
+    Short shortValueBox = Short.valueOf(shortValue);
+    Integer integerValueBox = Integer.valueOf(intValue);
+    Long longValueBox = Long.valueOf(longValue);
+
+    Float floatValueBox = Float.valueOf(floatValue);
+    Double doubleValueBox = Double.valueOf(doubleValue);
+
+    Character characterValueBox = Character.valueOf(charValue);
+    Boolean booleanValueBox = Boolean.valueOf(booleanValue);
+
+    public static void main(String[] args) {
+
+    }
+}
+*/
+
 
