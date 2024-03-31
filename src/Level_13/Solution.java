0991543de13641bb373e93d5355b42b50e109f36
Efim Goncharov <efim.goncharov@bk.ru>
2024-03-31 23:39:39 +0500

    13 lvl

--- src/Level_13/Solution.java
+++ src/Level_13/Solution.java
@@ -249,4 +249,21 @@ public class Solution {
 }
 */
 
+//ДВОЙНЫЕ ФИГУРНЫЕ СКОБКИ
+/*
+import java.util.ArrayList;
+public class Solution {
+
+    public static void main(String[] args) {
+        var strings = new ArrayList<String>() {{
+            add("Так");
+            add("тоже");
+            add("можно");
+            add("делать");
+            add("!");
+        }};
+    }
+}
+*/
+
 
