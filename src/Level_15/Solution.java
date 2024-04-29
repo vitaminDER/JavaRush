910a24e7b7a8b820a3435148586ede903ef73754
Efim Goncharov <efim.goncharov@bk.ru>
2024-04-29 14:45:09 +0500

    15 lvl

--- src/Level_15/Solution.java
+++ src/Level_15/Solution.java
@@ -193,4 +193,28 @@ class Solution {
 }
 */
 
+//ПРОГЛАТЫВАЕМ ИСКЛЮЧЕНИЕ
+/*
+public class Solution {
+
+    public static String errorMessage = "не повезло";
+
+    public static void main(String[] args) {
+        try {
+            generateLuckyNumber();
+        } catch (Exception e) {
+            System.out.println(errorMessage);
+        }
+    }
+
+    static void generateLuckyNumber() throws Exception {
+        int luckyNumber = (int) (Math.random() * 100);
+        if (luckyNumber == 13) {
+            throw new Exception();
+        }
+        System.out.println("твое счастливое число: " + luckyNumber);
+    }
+}
+*/
+
 
