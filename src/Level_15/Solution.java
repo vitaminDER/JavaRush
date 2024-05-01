1224c3e584c3ffa3bf4254196f8e1be2382e3c0f
Efim Goncharov <efim.goncharov@bk.ru>
2024-05-01 18:39:57 +0500

    15 lvl

--- src/Level_15/Solution.java
+++ src/Level_15/Solution.java
@@ -217,4 +217,26 @@ public class Solution {
 }
 */
 
+//ГРУППИРОВКА ИСКЛЮЧЕНИЙ
+/*
+public class Solution {
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("Программа работает от забора");
+            Thread.sleep(1000);
+            System.out.println("до обеда");
+        } catch (NullPointerException  | NumberFormatException e) {
+            System.out.println("Произошло исключение на букву N");
+        } catch (IllegalArgumentException  | IllegalStateException  | InterruptedException e) {
+            System.out.println("Произошло исключение на букву I");
+        }catch (ClassCastException e) {
+            System.out.println("Произошло исключение на букву C");
+        }
+
+    }
+
+}
+*/
+
 
