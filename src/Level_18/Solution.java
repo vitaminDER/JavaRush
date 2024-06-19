ed2f28e7f433d1e438132740ef27a4b90ae9d3de
Efim Goncharov <efim.goncharov@bk.ru>
2024-06-19 20:36:15 +0500

    18 lvl

---
+++ src/Level_18/Solution.java
@@ -0,0 +1,24 @@
+package Level_18;
+
+//Хищники vs Травоядные
+/*
+public class Solution {
+    public static void main(String[] args) {
+        printRation(new Cow());
+        printRation(new Lion());
+        printRation(new Elephant());
+        printRation(new Wolf());
+    }
+
+    public static void printRation(Animal animal){
+        String herbivore = "Любит траву";
+        String predator = "Любит мясо";
+        if(animal instanceof Herbivore && animal instanceof Herbivore ) {
+            System.out.println(herbivore);
+        }
+        if(animal instanceof Predator && animal instanceof Predator) {
+            System.out.println(predator);
+        }
+    }
+}
+*/
